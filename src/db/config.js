55defe16cb3654c057e70c054308045c58455281
DiegoEstela <die.estela@gmail.com>
2023-05-24 20:30:46 +0200

    test endpoint

--- src/db/config.js
+++ src/db/config.js
@@ -3,10 +3,6 @@ config();
 
 module.exports = {
   db: {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
+    vercel: process.env.POSTGRES_URL,
   },
 };
