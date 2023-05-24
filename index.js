5e3d2f543d4cc702c452fd2ac33c1d36932dcd20
DiegoEstela <die.estela@gmail.com>
2023-05-24 23:00:14 +0200

    the end

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const app = require("./src/app");
+const express = require("express");
+const cors = require("cors");
+const taskRoutes = require("./src/routes/task.routes");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(taskRoutes);
+
+app.use((err, req, res, next) => {
+  return res.json({
+    message: err.message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor http escuchando en el puerto ${PORT}`);
