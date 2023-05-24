b174a22017bf70cc249734e8b758362d4c1c7f24
DiegoEstela <die.estela@gmail.com>
2023-05-24 22:43:37 +0200

    reset

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const morgan = require("morgan");
+
 const cors = require("cors");
 const taskRoutes = require("./routes/task.routes");
 
 const app = express();
-app.use(morgan("dev"));
+
 app.use(cors());
 app.use(express.json());
 app.use(taskRoutes);
